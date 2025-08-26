d6ded5f280cdf5ad90e7412bfaa69e9a50a4eed6
nums <honzik.klusek.myto@gmail.com>
2025-08-26 22:01:48 +0200

    funkci add task

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import { handleButtonClick } from './leftsection.js'
 import { addproject } from './leftsection.js'
 import { closeedit } from './leftsection.js'
 import { deletetaskright } from './rightsection.js'
+import { addtaskright } from './rightsection.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     deletetask();
     editt();
     addproject();
     closeedit();
-    deletetaskright();    
+    deletetaskright();   
+    addtaskright(); 
 
 });
--- src/rightsection.js
+++ src/rightsection.js
@@ -1,4 +1,37 @@
-functoin
+export function addtaskright(){
+    const addtaskbtn = document.querySelector('.addtask');
+    const projectright = document.querySelector('.tasks');
+
+    addtaskbtn.addEventListener('click', () =>{
+        const project = document.createElement('div');
+        project.classList.add('tasks');
+        console.log('tohlejde')
+        projectright.appendChild(project)
+        project.innerHTML = ` 
+        <div class="todoitem">
+        
+        <div class="leftsectionleft">
+          <input type="checkbox" class="hotovo">
+          <p class="nametask">nazev tasku</p>
+        </div>
+        <div class="rightsecright">
+          <p class="podnadpisright">dva dny zbyvaji</p>
+          <span class="editright">
+            <img src="assets/Vector.png">
+          </span>
+          
+
+        <span class="deleteright">
+          <img src="assets/delete.png"> 
+        </span>
+          
+        </div>
+         
+      </div>
+`
+
+    });
+}
 
 export function deletetaskright(){
     const todolist = document.querySelector('.right-section'); 
