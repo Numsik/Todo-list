5224f0553cd86a697fce7fe201dc8cf8215f40be
nums <honzik.klusek.myto@gmail.com>
2025-08-30 17:41:05 +0200

    nejaky update

--- src/leftsection.js
+++ src/leftsection.js
@@ -1,33 +1,4 @@
-const todolist = {
-  Projects: [
-    {
-      projectName: "Škola",
-      todos: [
-        {
-          name: "Dopsat seminárku",
-          ddmmyyyy: "25082025",
-          isDone: false
-        },
-        {
-          name: "Naučit se na test",
-          ddmmyyyy: "26082025",
-          isDone: true
-        }
-      ]
-    },
-
-    {
-      projectName: "Práce",
-      todos: [
-        {
-          name: "Dokončit prezentaci",
-          ddmmyyyy: "27082025",
-          isDone: false
-        }
-      ]
-    }
-  ]
-}
+import { todolist } from "./todolist.js";
 export function addproject(){
     const addbtn = document.querySelector('.addproject');
     const projectcontent = document.querySelector('.project-content')
---
+++ src/todolist.js
@@ -0,0 +1,30 @@
+export const todolist = {
+  Projects: [
+    {
+      projectName: "Škola",
+      todos: [
+        {
+          name: "Dopsat seminárku",
+          ddmmyyyy: "25082025",
+          isDone: false
+        },
+        {
+          name: "Naučit se na test",
+          ddmmyyyy: "26082025",
+          isDone: true
+        }
+      ]
+    },
+
+    {
+      projectName: "Práce",
+      todos: [
+        {
+          name: "Dokončit prezentaci",
+          ddmmyyyy: "27082025",
+          isDone: false
+        }
+      ]
+    }
+  ]
+}
